23ee0953c305787e2f638fa2c5ecff3bdadb2f7f
Aniket Bansode <aniket.bansode2311@gmail.com>
2025-03-19 14:32:15 +0530

    initial commit

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Logo from "../moralis-logo.svg";
 import Eth from "../eth.svg";
 import { Link } from "react-router-dom";
 
@@ -10,7 +9,21 @@ function Header(props) {
   return (
     <header>
       <div className="leftH">
-        <img src={Logo} alt="logo" className="logo" />
+        <div className="logo-container">
+          <svg 
+            width="50" 
+            height="50" 
+            viewBox="0 0 300 300" 
+            fill="none" 
+            xmlns="http://www.w3.org/2000/svg"
+            className="logo"
+          >
+            <rect width="300" height="100" fill="#333333" />
+            <rect x="100" y="100" width="100" height="200" fill="#333333" />
+            <rect x="50" y="100" width="50" height="25" fill="#333333" />
+            <rect x="200" y="100" width="50" height="25" fill="#333333" />
+          </svg>
+        </div>
         <Link to="/" className="link">
           <div className="headerItem">Swap</div>
         </Link>
